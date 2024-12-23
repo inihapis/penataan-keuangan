dad5a356a512193450cfda71ca08fd7a806516ff
Noneng <pitrinoneng@gmail.com>
2024-12-23 15:28:20 +0700

    update 23 dees

--- index.php
+++ index.php
@@ -169,7 +169,7 @@ $tujuhhari= mysqli_fetch_array($tujuhhari);
 </div>
 
 
-            <!-- Earnings (Monthly) Card Example -->
+            <!-- Earnings (Monthly) Card  Example -->
             <div class="col-xl-3 col-md-6 mb-4">
     <div class="card border-left-danger shadow h-100 py-2">
         <div class="card-body">
